603981e62157cddbea9b32dd18c14048317d6be9
Dmitrii Krasnoperov <d.krasnoperov@codedrop.cc>
2019-05-31 18:41:56 +0600

    Add configure counts of block. 'Delete block' button for all panel, instead of one button, names are not repated now

--- script.js
+++ script.js
@@ -133,9 +133,21 @@ $(document).ready(function() {
     "whippet",
     "wolfhound/irish"
   ]
+  getDoggysList("https://dog.ceo/api/breeds/list/all");
+
+  function getDoggysList(link) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', link, false);
+    xhr.send();
+    let lss = JSON.parse(xhr.responseText);
+    //alert( lss.message );
+  }
+
+
   let accordion = $(".accordion");
   let ul = accordion.find("ul");
   let id = 0;
+  let key = 0;
   let doggys = [];
 
   for (let i = 0; i < 4; i++) {
@@ -143,62 +155,88 @@ $(document).ready(function() {
   }
 
   $("#create").on( "click", function() {
-    createBlock();
+    createBlocks();
   });
   $("#remove").on( "click", function() {
     removeBlock();
   });
-  
-  
 
-  function removeBlock() {
-    ul.find("#" + (id - 1)).remove();
-    id--;
+  function createBlocks() {
+    let countCreate = 0;
+    let inputValue = $(".input_text").val();
+    if (!isNaN(inputValue)) countCreate = inputValue;
+    for (let i = 0; i < countCreate; i++) {
+      createBlock();
+    }
   }
-
+  
   function createBlock() {
-
     let newDoggy = doggys_list[Math.round(Math.random() * doggys_list.length)];
+    
+    doggys_list.splice( doggys_list.indexOf(newDoggy), 1 );
+
     let nameDoggy = newDoggy[0].toUpperCase() + newDoggy.slice(1);
     if (nameDoggy.indexOf("/") >= 1) {
       nameDoggy = nameDoggy.substring(0, nameDoggy.indexOf("/")) + ' ' + nameDoggy.substr(nameDoggy.indexOf("/") + 1);
     }
-    doggys[id] = {};
-    doggys[id].id = id;
-    doggys[id].link = newDoggy;
-    doggys[id].name = nameDoggy;
+    doggys[key] = {};
+    doggys[key].id = id;
+    doggys[key].link = newDoggy;
+    doggys[key].name = nameDoggy;
 
-    ul.append("<li id='" + doggys[id].id + 
+    ul.append("<li id='" + doggys[key].id + 
     "'><input type='checkbox' class='accordion_button'><i></i><h2>" +
-      doggys[id].name + "</h2><div class='panel'><div class='images'></div>" +
-     "<input type='button' value='Добавить картинку' class='button addImg'></div></li>");
+      doggys[key].name + "</h2><div class='panel'><div class='images'></div>" +
+     "<div class='accoridon__block__buttons'><input type='button' value='Добавить картинку' class='button addImg'>" +
+     "<input type='button' value='Убрать блок' id='remove' class='button removeBlock'></div></div></li>");
 
-    getRandomImage("https://dog.ceo/api/breed/" + doggys[id].link + "/images/random", doggys[id].id);
-    
-    $(".addImg").unbind("click").on( "click", function() {
-      let parentId = $(this).parent().parent().attr("id");
-      let counts = 2;
-      counts = countImages(parentId);
-      for (let i = 0; i < counts; i++) {
-        getRandomImage("https://dog.ceo/api/breed/" + 
-        doggys[parentId].link + 
-        "/images/random", parentId);
-      }
+    getRandomImage("https://dog.ceo/api/breed/" + doggys[key].link + "/images/random", doggys[key].id);
+  
+    let button__addImg = ul.find("#" + doggys[key].id + " .addImg");
+    button__addImg.on( "click", function() {
+      addImg($(this).parent().parent().parent().attr("id"));
     });
-
+    let button__removeBlock = ul.find("#" + doggys[key].id + " .removeBlock");
+    button__removeBlock.on( "click", function() {
+      removeBlock($(this).parent().parent().parent().attr("id"));
+    });
     $(".accordion_button").unbind("click").on( "click", function() {
       toggleAccordion($(this).parent().attr("id"));
     });
-
     id++;
+    key++;
   }
 
+  function findKey(thisId) { 
+    let i = 0;
+    let thisKey = 0;
+    doggys.forEach(element => {
+      if (element.id == thisId) {
+        thisKey = i;
+      }
+      i++;
+    });
+    return thisKey;
+  }
 
-  function countImages(thisId) {
+  function addImg(thisId) {
+    let counts = 2;
+    let thisKey = 0;
+    thisKey = findKey(thisId);
+
+    counts = countImages(thisId, thisKey);
+    for (let i = 0; i < counts; i++) {
+      getRandomImage("https://dog.ceo/api/breed/" + 
+      doggys[thisKey].link + 
+      "/images/random", thisId);
+    }
+  }
+
+  function countImages(thisId, position) {
     let thisLi = $("#" + thisId);
     let thisButton = thisLi.find(".addImg");
     if (!thisButton.hasClass("active")) {
-      counts = (+thisId + 1) * 2;
+      counts = (+position + 1) * 2;
     } else {
       counts = 2;
     }
@@ -206,6 +244,14 @@ $(document).ready(function() {
       thisButton.toggleClass("active");
     }
     return counts;
+  }
+
+  function removeBlock(thisId) {
+    ul.find("#" + (thisId)).remove();
+    thisKey = findKey(thisId);
+    doggys_list.push(doggys[thisKey].link);
+    doggys.splice(thisId, 1);
+    key--;
   }
 
   function toggleAccordion(thisId) {
