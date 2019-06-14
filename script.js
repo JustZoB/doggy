a7aae616454081a638659a21452dd027c272a263
Dmitrii Krasnoperov <d.krasnoperov@codedrop.cc>
2019-06-14 12:42:18 +0600

    Add localStorage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
   let doggys_list = getDoggysList("https://dog.ceo/api/breeds/list/all");
 
   function getDoggysList(link) {
@@ -18,47 +18,79 @@ $(document).ready(function() {
     }
     return doggys_list;
   }
-
+  //localStorage.clear();
   // События для кнопок
-  $("#create-accordion").on( "click", function() {
+  document.getElementById('create-accordion').onclick = function() {
     createAccordion();
-  });
+  };
+
+  /*document.querySelectorAll(".create-block").addEventListener('click', function() {
+    createBlocks();
+  });*/
+
+  //-----------------------------------//
   $("body").on( "click", ".create-block", function() {
     createBlocks($(this).parents().eq(1).attr("data-ac-id"));
   });
   $("body").on( "click", ".addImg", function() {
-    addImg($(this).parents().eq(4), $(this).parents().eq(2).attr("data-id"), $(this).attr("data-doggy"));
+    addImg($(this).parents().eq(5).attr("data-ac-id"),
+     $(this).parents().eq(2).attr("data-id"), $(this).attr("data-doggy"));
   });
   $("body").on( "click", ".removeBlock", function() {
     removeBlock($(this).parents().eq(2).attr("data-id"), $(this).attr("data-doggy"));
   });
-
-  let container = $(".container");
+  //-------------------------//
+  let container = $(".container"); //------------
   let ac_count = -1;
   let id = 0;
-
-  createAccordion();
-  for (let i = 0; i < 3; i++) {
+  let c = 0;
+  if (localStorage.length == 0) {
+    createAccordion();
+    for (let i = 0; i < 3; i++) {
+      createBlock(ac_count);
+    }
+  } else {
+    for (let key in localStorage) {
+      if (key == "ac_count") {
+        for (let i = 0; i <= localStorage[key]; i++) {
+          htmlAccordion(i);
+        }
+        ac_count = localStorage[key];
+      } 
+    }
+    for (let key in localStorage) {
+      if (c < localStorage.length) {
+        let newImagesForDoggy = JSON.parse(localStorage.getItem(key));
+        if (newImagesForDoggy.ac != undefined) {
+          let ac = newImagesForDoggy.ac;
+          let name = newImagesForDoggy.name;
+          let images = newImagesForDoggy.images;
+          createLocalBlock(ac, name, images);
+        }
+        c++;
+      }
+    }
+  }
+  
+  function createAccordion() {
+    ac_count++;
+    localStorage.setItem("ac_count", ac_count);
+    htmlAccordion(ac_count);
     createBlock(ac_count);
   }
 
-  function createAccordion() {
-    ac_count++;
-
-    container.append("<div data-ac-id='" + ac_count + "'></div");
-    let newAc = container.find("[data-ac-id=" + ac_count +"]");
+  function htmlAccordion(ac_id) {
+    container.append("<div data-ac-id='" + ac_id + "'></div");
+    let newAc = container.find("[data-ac-id=" + ac_id +"]");
     newAc.append("<div class='buttons__block'></div>");
     let buttons__block = newAc.find(".buttons__block");
-    buttons__block.append("<input data-input-ac-id='" + ac_count + "' type='button' value='Создать блоков' class='button create-block'>");
+    buttons__block.append("<input data-input-ac-id='" + ac_id + "' type='button' value='Создать блоков' class='button create-block'>");
     buttons__block.append("<input type='text' value='1' class='input_text'>");
     newAc.append("<div class='accordion'><ul></ul></div>");
-
-    createBlock(ac_count);
-    
   }
 
   function createBlocks(ac_id) {
-    let currunt_ac = container.find("[data-ac-id='" + ac_id)
+    let currunt_ac = container.find("[data-ac-id='" + ac_id + "']");
     let countCreate = 0;
     let inputValue = currunt_ac.find(".input_text").val();
     if (!isNaN(inputValue)) countCreate = inputValue;
@@ -66,18 +98,55 @@ $(document).ready(function() {
       createBlock(ac_id);
     }
   }
-  
-  function createBlock(ac_id) {
-    let newDoggy = doggys_list[Math.round(Math.random() * doggys_list.length)];
-    doggys_list.splice(doggys_list.indexOf(newDoggy), 1);
 
+  function createLocalBlock(ac_id, newDoggy, images) {
+    doggys_list.splice(doggys_list.indexOf(newDoggy), 1);
     let nameDoggy = newDoggy[0].toUpperCase() + newDoggy.slice(1);
     if (nameDoggy.indexOf("/") >= 1) {
       nameDoggy = nameDoggy.substring(0, nameDoggy.indexOf("/")) + 
       ' ' + nameDoggy.substr(nameDoggy.indexOf("/") + 1);
     }
 
-    let currunt_ac = $("[data-ac-id='" + ac_id + "']");
+    htmlBlock(ac_id, nameDoggy, newDoggy);
+
+    let currunt_ac = container.find("[data-ac-id='" + ac_id + "']");
+    let ul = currunt_ac.find("ul");
+    let li_id = id;
+    let li = ul.find("[data-id=" + li_id +"]");
+     
+    for (let i = 0; i < images.length; i++) {
+      let blockImages = li.find(".images");
+      blockImages.append("<img src='" + images[i] +"'>");
+    }
+
+    id++;
+  }
+
+  function createBlock(ac_id) {
+    let newDoggy = doggys_list[Math.round(Math.random() * doggys_list.length)];
+
+    doggys_list.splice(doggys_list.indexOf(newDoggy), 1);
+    let nameDoggy = newDoggy[0].toUpperCase() + newDoggy.slice(1);
+    if (nameDoggy.indexOf("/") >= 1) {
+      nameDoggy = nameDoggy.substring(0, nameDoggy.indexOf("/")) + 
+      ' ' + nameDoggy.substr(nameDoggy.indexOf("/") + 1);
+    }
+
+    htmlBlock(ac_id, nameDoggy, newDoggy);
+
+    let newLocalDog = {}; 
+    newLocalDog.ac = ac_id; 
+    newLocalDog.name = newDoggy;
+    newLocalDog.images = [];
+
+    newLocalDog.images.push(getRandomImage(id, "https://dog.ceo/api/breed/" + newDoggy + "/images/random"));
+    localStorage.setItem(newDoggy, JSON.stringify(newLocalDog));
+
+    id++;
+  }
+
+  function htmlBlock(ac_id, nameDoggy, newDoggy) {
+    let currunt_ac = container.find("[data-ac-id='" + ac_id + "']");
     let ul = currunt_ac.find("ul");
     let li_id = id;
     let input_id = "input_" + id;
@@ -94,29 +163,28 @@ $(document).ready(function() {
     let ac_block_buttons = panel.find(".accoridon__block__buttons");
     ac_block_buttons.append("<input data-doggy='" + newDoggy + "' type='button' value='Добавить картинку' class='button addImg'>");
     ac_block_buttons.append("<input data-doggy='" + newDoggy + "' type='button' value='Убрать блок' class='button removeBlock'>");
-
-    getRandomImage(id, "https://dog.ceo/api/breed/" + newDoggy + "/images/random");
-
-    id++;
   }
 
   function addImg(ac_id, id, doggy) {
     let counts = 2;
     let position = 0;
+    let newImagesForDoggy = JSON.parse(localStorage.getItem(doggy));
     position = findPosition(ac_id, id);
     counts = countImages(id, position);
     for (let i = 0; i < counts; i++) {
-      getRandomImage(id, "https://dog.ceo/api/breed/" + doggy + "/images/random");
+      newImagesForDoggy.images.push(getRandomImage(id, "https://dog.ceo/api/breed/" + doggy + "/images/random")); //
     }
+    localStorage.setItem(doggy, JSON.stringify(newImagesForDoggy));
   }
 
   function removeBlock(id, doggy) {
     container.find("[data-id='" + id + "']").remove();
+    localStorage.removeItem(doggy);
     doggys_list.push(doggy);
   }
 
   function findPosition(ac_id, id) { 
-    let ac = $("[data-ac-id='" + ac_id + "']");
+    let ac = container.find("[data-ac-id='" + ac_id + "']");
     let lis = ac.find("li");
     let li = ac.find("[data-id='" + id + "']");
     let pos = 0;
@@ -125,7 +193,7 @@ $(document).ready(function() {
   }
 
   function countImages(id, position) {
-    let li = $("[data-id='" + id + "']");
+    let li = container.find("[data-id='" + id + "']");
     let button = li.find(".addImg");
     counts = 2;
     if (!button.hasClass("active")) {
@@ -145,5 +213,6 @@ $(document).ready(function() {
     let li = container.find("[data-id='" + id + "']");
     let blockImages = li.find(".images");
     blockImages.append("<img src='" + obj.message +"'>");
+    return obj.message;
   }
 });
